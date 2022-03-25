8a2d40ef7dcaa5d14e616d46167e12f901513f49
AliOsman <79115868+alosoz@users.noreply.github.com>
2022-03-25 22:31:33 +0100

    son hali

--- question2.py
+++ question2.py
@@ -4,3 +4,22 @@
 # If the average is less than 50, “FAILED” on the screen, 50 and above, “PASSED” will be printed on the screen. 
 # This printing process is in 4 lessons. will be done and the lessons will be written one after the other.
 
+
+
+user_name = input("Name: ") 
+surname = input("Surname: ")
+student_number = input("Student Number: ") 
+
+course = ["Art","History","Science","Math"]
+
+
+for lesson in course:
+    visa = float(input(lesson +" Visa: "))
+    final = float(input(lesson +" Final: "))
+
+    year_average = ((visa*40)/100) + ((final*60)/100)
+    
+    if year_average < 50:
+        print(lesson + ": Failed")
+    elif year_average >= 50:
+        print(lesson + ": Passed")
